58e54105af2e0caa0001beb466806230441084e1
Renato Leal <marcondesleal@icloud.com>
2022-02-07 17:53:46 -0300

    Responsividade e adaptabilidade
    Determinando as questões de responsividade e adaptabilidade do App.

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -17,13 +17,59 @@ class HomePage extends StatelessWidget {
           Container(
             height: availableHeight * 0.10,
             color: Colors.deepPurple[200],
+            child: LayoutBuilder(
+              builder: (context, constraints) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: constraints.maxHeight,
+                    width: constraints.maxWidth * 0.40,
+                    color: Colors.white,
+                    child: Center(child: Text("Site Oficial")),
+                  ),
+                  Container(
+                    height: constraints.maxHeight,
+                    width: constraints.maxWidth * 0.20,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Icon(
+                        Icons.account_circle_rounded,
+                        size: constraints.maxHeight * 0.9,
+                        color: Color(0xFF000000),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
-            height: availableHeight * 0.10,
+            height: (availableHeight * 0.08),
             color: Colors.deepPurple[400],
+            child: LayoutBuilder(
+              builder: (context, constraints) => Row(
+                children: [
+                  Container(
+                    color: Colors.grey[300],
+                    height: constraints.maxHeight,
+                    width: (constraints.maxWidth) * (1 / 3),
+                  ),
+                  Container(
+                    color: Colors.grey[500],
+                    height: constraints.maxHeight,
+                    width: (constraints.maxWidth) * (1 / 3),
+                  ),
+                  Container(
+                    color: Colors.grey[700],
+                    height: constraints.maxHeight,
+                    width: (constraints.maxWidth) * (1 / 3),
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
-            height: availableHeight * 0.80,
+            height: availableHeight * 0.82,
             color: Colors.deepPurple[600],
           ),
         ],
